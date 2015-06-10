753de5ea8154ea170b02d6af7198ac0446115e69
Ron Williams <ron.a.williams@gmail.com>
2015-06-09 21:22:45 -0700

    moar clean

--- src/JsonWriter.js
+++ src/JsonWriter.js
@@ -1,7 +1,6 @@
 Ext.define('CouchDB.data.Writer', {
     extend: 'Ext.data.writer.Json',
     alias: 'writer.couchdb',
-
     allowSingle: true,
     encode: false,
     writeAllFields: true,
@@ -9,11 +8,7 @@ Ext.define('CouchDB.data.Writer', {
     clientIdProperty: '_id',
     transform: {
         fn: function(data,request) {
-
-            Ext.Object.each(request._records[0].getAssociatedData(), function(key, value, myself) {
-                data[key] = value;
-            });
-
+            request.getRecords()[0].getAssociatedData(data);
             return data;
         },
         scope: this
